357c53bbf6d524ce7ded369065b3c836dff955b3
dylan-perk04 <dylan.perkins@my.jcu.edu.au>
2023-09-22 13:50:08 +1000

    Refactor password check program to use functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,7 +1,19 @@
-password_length = int(input("What is the length of the password: "))
-password = input("What is the password: ")
-while len(password) < password_length:
-    print("Password doesn't meet length requirement")
-    password = input("What is the password: ")
+def main():
+    password_length = int(input("What is the length of the password: "))
+    password = get_password(password_length)
+    print_asterisks(password)
 
-print("*" * len(password))
+
+def get_password(password_length):
+    password = input("What is the password: ")
+    while len(password) < password_length:
+        print("Password doesn't meet length requirement")
+        password = input("What is the password: ")
+    return password
+
+
+def print_asterisks(password):
+    print("*" * len(password))
+
+
+main()
