7de4c49135701ebc15dbfdaf134dab744565210c
dylan-perk04 <dylan.perkins@my.jcu.edu.au>
2023-10-06 16:30:04 +1000

    Update variable name in display_subject_details function

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -9,10 +9,12 @@ FILENAME = "subject_data.txt"
 def main():
     data = get_data()
     print(data)
+    display_subject_details(data)
 
 
 def get_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
+    data = []
     input_file = open(FILENAME)
     for line in input_file:
         print(line)  # See what a line looks like
@@ -23,7 +25,15 @@ def get_data():
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
         print(parts)  # See if that worked
         print("----------")
+        data.append(parts)
     input_file.close()
+    return data
+
+
+def display_subject_details(data):
+    """Display subject details by subject, lecturer and number of students."""
+    for datum in data:
+        print(f"{datum[0]} is taught by {datum[1]} and has {datum[2]} students")
 
 
 main()
