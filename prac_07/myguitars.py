0087a98ae98b45e952d0f3cdf2454c37b9874b41
dylan-perk04 <dylan.perkins@my.jcu.edu.au>
2023-11-03 17:10:24 +1000

    Complete More Guitars exercise

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -8,18 +8,41 @@ COST_INDEX = 2
 
 def main():
     guitars = []
+    load_file(guitars)
 
-    with open(FILENAME, 'r') as out_file:
-        for line in out_file:
-            parts = line.strip().split(',')
-            parts[YEAR_INDEX] = int(parts[YEAR_INDEX])
-            parts[COST_INDEX] = float(parts[COST_INDEX])
-            guitar_to_add = Guitar(parts[NAME_INDEX], parts[YEAR_INDEX], parts[COST_INDEX])
-            guitars.append(guitar_to_add)
+    name = input("Name: ")
+    while name != "":
+        year = int(input("Year: "))
+        cost = float(input("Cost: $"))
+        guitar_to_add = Guitar(name, year, cost)
+        guitars.append(guitar_to_add)
+        print(f"{guitar_to_add} added")
+        print()
+        name = input("Name: ")
 
     guitars.sort()
     for guitar in guitars:
         print(guitar)
+
+    save_file(guitars)
+
+
+def load_file(guitars):
+    with open(FILENAME, 'r') as out_file:
+        for line in out_file:
+            parts = line.strip().split(',')
+            name = parts[NAME_INDEX]
+            year = int(parts[YEAR_INDEX])
+            cost = float(parts[YEAR_INDEX])
+            guitar_to_add = Guitar(name, year, cost)
+            guitars.append(guitar_to_add)
+
+
+def save_file(guitars):
+    with open(FILENAME, 'w') as out_file:
+        for guitar in guitars:
+            parts = [guitar.name, str(guitar.year), str(guitar.cost)]
+            print(','.join(parts), file=out_file)
 
 
 main()
